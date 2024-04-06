0ab70b2744521b36aca85d29d0100020f5f0a74c
Nonso Anetoh <anet0001@algonquinlive.com>
2024-04-06 18:42:36 -0400

    updates: app.js file

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,19 @@
 class App {
   constructor() {
-    console.log('With Love - ACTTA Studio');
+    console.log('Made With Love - ACTTA Studio');
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    const links = document.querySelectorAll('a');
+
+    links.forEach((link) => {
+      link.onclick = (event) => {
+        event.preventDefault();
+        console.log(event);
+      };
+    });
   }
 }
 
-App();
+new App();
