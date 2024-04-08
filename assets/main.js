d5a852a766219e23e729fa0aa543a24b215e841c
Nonso Anetoh <anet0001@algonquinlive.com>
2024-04-08 15:00:29 -0400

    updates: main.js

---
+++ assets/main.js
@@ -0,0 +1,1 @@
+class t{constructor(){console.log("Initializing Text Animation")}}new class{constructor(){console.log("Starting My Application"),this.initTextAnimation()}initTextAnimation(){this.text=new t}};
